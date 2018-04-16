8df431d5678684c9065c175d12f8242862d74168
Alyssa Biot <alyssa.biot@gmail.com>
2018-04-16 22:38:58 +0200

    first tests with api - get categories, products, detailed infos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def index
+    @categories = Openfoodfacts::Category.all(locale: 'fr').first(10).pluck(:name)
+    @category_name = Openfoodfacts::Category.all(locale: 'fr').first.name
+
+    @products_from_category = Openfoodfacts::Product.search(@category_name, locale: 'fr', page_size: 1).first(10)
+
+    code = @products_from_category.first._id
+    @product = Openfoodfacts::Product.get(code, locale: 'fr')
+    @product_name = @product.product_name
+    @product_nutriments = @product.nutriments
+
+    #@additives = Openfoodfacts::Additive.all(locale: 'fr').first(10).pluck(:name)
+
+    #@palm = Openfoodfacts::IngredientThatMayBeFromPalmOil.all(locale: 'fr').first(10).pluck(:name)
   end
 end
