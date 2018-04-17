06959a8012ae71cb8f3cb8375b3e9b2bc35bbfe2
Alyssa Biot <alyssa.biot@gmail.com>
2018-04-17 22:57:54 +0200

    first test with vue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,5 +6,6 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-
+import Test from 'tests'
+console.log(Test)
 console.log('Hello World from Webpacker')
